6ea9da2ad2767dad9231438c8f20de99f3299664
AryanRajdev <91838247+AryanRajdev@users.noreply.github.com>
2025-03-17 23:16:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function makeid(l) {
   // write your code here
+   let res = "";
+   const char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+   for(int i =0 ; i<l ; i++){
+	   res += charAt(
+		   Math.floor(Math.random()*char_list.length)
+	   )
+   }
+
+	return res;
 }
 
 // Do not change the code below.
